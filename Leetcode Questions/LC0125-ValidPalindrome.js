bcd879f40ec87e78d14febe699aa134bd05d0aa4
Ceci Tran <ceci@Cecis-MacBook-Pro.local>
2020-03-18 17:45:24 -0700

    another way to check palindrome

--- Leetcode Questions/LC0125-ValidPalindrome.js
+++ Leetcode Questions/LC0125-ValidPalindrome.js
@@ -11,3 +11,25 @@ var isPalindrome = function(s) {
 	let revStr = s.split("").reverse().join("").replace(/[^a-zA-Z0-9]/g, '').toLowerCase(); 
     return (revStr === s) ? true : false;
 }
+
+//using two pointers going to the middle
+var isPalindrome = function(s){
+    //edit string 
+    s = s.replace(/[^a-zA-Z0-9]/g, '');
+    s = s.toLowerCase();
+
+    //set two pointers
+    let start = 0;
+    let end = s.length - 1;
+    //white start pointer is < than the end point index
+    while (start <= end) {
+        //if the letter at start pointer != letter at end pointer
+        if (s[start] != s[end]) {
+            return false;
+        }
+        //increment if no problem
+        start++;
+        end--;
+    }
+    return true;
+}
